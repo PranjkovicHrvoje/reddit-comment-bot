9eb96552b25f0036b4e81c025c18a513d8c4a580
Hrvoje Pranjković <89313677+PranjkovicHrvoje@users.noreply.github.com>
2023-06-04 00:45:20 +0200

    Update reddit_bot.py

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,55 +1,56 @@
-import praw
-import config
-import time
-import os
+import praw, config, time, os
 
 def bot_login():
-	print "Logging in..."
+	print ('Logging in...')
 	r = praw.Reddit(username = config.username,
 				password = config.password,
 				client_id = config.client_id,
 				client_secret = config.client_secret,
 				user_agent = "The Reddit Commenter v1.0")
-	print "Logged in!"
+	print ('Logged in!')
 
 	return r
 
 def run_bot(r, comments_replied_to):
-	print "Searching last 1,000 comments"
+	print ('Searching last 200 comments')
 
-	for comment in r.subreddit('test').comments(limit=1000):
-		if "sample user comment" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
-			print "String with \"sample user comment\" found in comment " + comment.id
-			comment.reply("Hey, I like your comment!")
-			print "Replied to comment " + comment.id
+	for comment in r.subreddit('subreddit').comments(limit=200):
+		compareWith = ''
+		compareWith = compareWith.upper()
+		keyWords = comment.body
+		keyWords = keyWords.upper()
+		f = open('comments_replied_to.txt')
+		if ((compareWith in keyWords) and (comment.id not in comments_replied_to)):			
+			print (f'String with "{compareWith}" found in comment {comment.id}')
+			comment.reply('Reply text')
+			print (f'Replied to comment {comment.id}')
 
-			comments_replied_to.append(comment.id)
+			file1=open('comments_replied_to.txt', 'a')
+			file1.write(comment.id + '\n')
+			file1.close()
 
-			with open ("comments_replied_to.txt", "a") as f:
-				f.write(comment.id + "\n")
-
-	print "Search Completed."
-
-	print comments_replied_to
-
-	print "Sleeping for 10 seconds..."
+	print ('Search Completed.')
+	print (comments_replied_to)
+	print ('Sleeping for 10 seconds...')
 	#Sleep for 10 seconds...		
 	time.sleep(10)
 
 def get_saved_comments():
-	if not os.path.isfile("comments_replied_to.txt"):
+	if not os.path.isfile('comments_replied_to.txt'):
 		comments_replied_to = []
 	else:
-		with open("comments_replied_to.txt", "r") as f:
-			comments_replied_to = f.read()
-			comments_replied_to = comments_replied_to.split("\n")
-			comments_replied_to = filter(None, comments_replied_to)
+		f = open('comments_replied_to.txt', 'r')
+		comments_replied_to = f.read()
+		comments_replied_to = comments_replied_to.split('\n')
+		comments_replied_to = filter(None, comments_replied_to)
 
 	return comments_replied_to
 
 r = bot_login()
 comments_replied_to = get_saved_comments()
-print comments_replied_to
+print (comments_replied_to)
 
 while True:
+	comments_replied_to = get_saved_comments()
 	run_bot(r, comments_replied_to)
+	
